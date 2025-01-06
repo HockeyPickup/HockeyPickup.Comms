0f7d6256460623b5beb52285b4710bc44be8b490
Brett Morrison <b@brettmorrison.com>
2025-01-05 16:34:55 -0800

    Converted functions app to WebApplication

--- HockeyPickup.Comms/Program.cs
+++ HockeyPickup.Comms/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 using HockeyPickup.Comms.Services;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
         services.AddSingleton<IEmailService, EmailService>();

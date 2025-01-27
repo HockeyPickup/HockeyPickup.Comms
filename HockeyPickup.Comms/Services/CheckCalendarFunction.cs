a05b3a19dbe5a1621f85c5442589af1dc61e427d
Brett Morrison <b@brettmorrison.com>
2025-01-27 12:43:56 -0800

    Updated function schedule for CheckCalendar

--- HockeyPickup.Comms/Services/CheckCalendarFunction.cs
+++ HockeyPickup.Comms/Services/CheckCalendarFunction.cs
@@ -37,7 +37,7 @@ public class CheckCalendarFunction
 
     #region interface
     [Function("CheckCalendar")]
-    public async Task RunAsync([TimerTrigger("0 0 10 */4 *")] TimerInfo timerInfo)
+    public async Task RunAsync([TimerTrigger("0 0 10,14 * * *")] TimerInfo timerInfo)
     {
         _logger.LogInformation($"Timer trigger function executed: {timerInfo?.ScheduleStatus?.ToString()}");
 
